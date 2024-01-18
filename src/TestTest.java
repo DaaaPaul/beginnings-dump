4f1b6fb92506162d59c5715dec549349f936fc29
Paul Peng <paulpeng2008@gmail.com>
2024-01-18 16:31:54 -0500

    a

--- src/TestTest.java
+++ src/TestTest.java
@@ -1,11 +1,8 @@
+import java.util.ArrayList;
+
 public class TestTest {
     public static void main(String[] args) {
         int[] test = new int[5];
-        for(int i = 0; i < test.length; i++){
-            test[i] = 3;
-        }
-        for(int i = 0; i < test.length -1 ; i++){
-            System.out.println(test[i]);
+        System.out.println(test.length);
         }
     }
-}
